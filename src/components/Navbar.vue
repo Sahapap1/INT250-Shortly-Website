6e33e92fb004f08ad9da1ea0b97c33461db668e2
sahapapeuro@gmail.com <Student@Train11-2-PC357.control.ad.sit.kmutt.ac.th>
2026-02-11 17:09:04 +0700

    first init

---
+++ src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+</script>
+
+<template>
+    <!-- Nav Container -->
+    <nav >
+        <!-- Flex Container for all items -->
+        <div >
+            <!-- Flex Container for Logo/Menu -->
+            <div>
+                <!-- Logo -->
+                <img src="../assets/logo.svg" alt="">
+                <!-- Left Menu -->
+                <div>
+                    <a href="">Features</a>
+                    <a href="">Pricing</a>
+                    <a href="">Resources</a>
+                </div>
+
+                <!-- Right Button Menu -->
+                <div>
+                    <a href="">Login</a>
+                    <a href="">Sign up</a>
+                </div>
+
+
+                <!-- @todo Hamburger Button -->
+            </div>
+
+
+        </div>
+    </nav>
+
+</template>
+
+<style scope></style>
